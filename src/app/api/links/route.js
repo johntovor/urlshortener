c57246b84768457b281094e1892f3d1d7a845a3c
John Tovor <johntovor@gmail.com>
2023-08-22 02:30:24 +0000

    URL validation added

--- src/app/api/links/route.js
+++ src/app/api/links/route.js
@@ -1,6 +1,19 @@
 import { NextResponse } from "next/server";
 
+import isValidURL from "@/app/lib/isValidURL";
 
-export async function POST() {
-    return NextResponse.json({items: "Hi"});
+
+export async function POST(request) {
+    const data = await request.json()
+    const contentType = await request.headers.get("content-type");
+    if (contentType !== "application/json") {
+        return NextResponse.json({"error": "Invalid request"}, {status: 415});
+    }
+
+    const url = data && data.url ? data.url : null;
+    const validURL = await isValidURL(url, ["jref.io", "urlshortener-navy.vercel.app", process.env.NEXT_PUBLIC_VERCEL_URL]);
+    if (!validURL) {
+        return NextResponse.json({"message": `${url} is not valid.`}, {status: 400});
+    }
+    return NextResponse.json(data, {status: 201});
 }
